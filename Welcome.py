0cd7ff16a08b9b88081237890dc8419d9e68481f
FabianZah <131347011+FabianZah@users.noreply.github.com>
2023-08-03 12:18:23 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -15,21 +15,25 @@ st.write('Hello World')
 df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
-# file_name = st.multiselect('select location', file_name_list, file_name_list[0])
-file_name = st.selectbox('select location', file_name_list)
-df = pd.read_csv(file_name)
+col1, col2 = st.columns(2)
 
-el_list = df.columns.tolist()[27:80]
-x_axis = st.selectbox('select x axis', el_list)
-y_axis = st.selectbox('select y axis', el_list)
-
-x = df[x_axis]/10000 
-y = df[y_axis]/10000
-
-p = figure(
-    x_axis_label=x_axis,
-    y_axis_label=y_axis)
-
-p.circle(x, y)
-
-st.bokeh_chart(p, use_container_width=True)
+with col1:
+   st.header("Selection")
+  # file_name = st.multiselect('select location', file_name_list, file_name_list[0])
+  file_name = st.selectbox('select location', file_name_list)
+  df = pd.read_csv(file_name)
+  
+  el_list = df.columns.tolist()[27:80]
+  x_axis = st.selectbox('select x axis', el_list)
+  y_axis = st.selectbox('select y axis', el_list)
+with col2:
+  x = df[x_axis]/10000 
+  y = df[y_axis]/10000
+  
+  p = figure(
+      x_axis_label=x_axis,
+      y_axis_label=y_axis)
+  
+  p.circle(x, y)
+  
+  st.bokeh_chart(p, use_container_width=True)
