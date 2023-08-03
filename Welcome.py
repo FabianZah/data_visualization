d0cb45215b3608731e6ceb078b899c2e3c542055
FabianZah <131347011+FabianZah@users.noreply.github.com>
2023-08-03 10:34:34 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -16,12 +16,13 @@ df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
-x_axis = st.selectbox('select element', el_list)
+x_axis = st.selectbox('select x axis', el_list)
+y_axis = st.selectbox('select y axis', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])
 
 x = df[x_axis]
-y = df['Si']
+y = df[y_axis]
 
 p = figure(
     x_axis_label='x',
